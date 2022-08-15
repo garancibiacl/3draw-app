c05104e75f092d84286df2397c15a761d0bc45b4
Gustavo <garancibiacl@gmail.com>
2022-08-14 20:44:15 -0400

    Se cambia title

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>React first app</h1>
+     <h1>React first app de productos modelados en 3D</h1>
     </div>
   );
 }
