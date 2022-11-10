0bf91553ffcdb5b5dbc49765022c8943dbcfb098
Gustavo <garancibiacl@gmail.com>
2022-11-09 22:55:10 -0300

    se ajusta spinner center

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,6 +1,6 @@
 import React,{ useEffect, useState } from 'react'
 import { ItemDetail } from '../ItemDetail/ItemDetail'
-import { Spinners } from '../Spinners/Spinners'
+import { Dna } from  'react-loader-spinner'
 import { useParams } from 'react-router-dom';
 import drawdata from '../../data/drawdata';
 
@@ -9,7 +9,7 @@ export const ItemDetailContainer = () => {
     const [producto, setProducto] = useState({})
     const [isLoading, setIsLoading] = useState(true);
     const getProductos = () => new Promise((resolve, reject) => {
-        setTimeout(()=> resolve(drawdata.find(product => product.id === Number(id) )), 1000)
+        setTimeout(()=> resolve(drawdata.find(product => product.id === Number(id) )), 2000)
         
       })
 
@@ -21,15 +21,26 @@ export const ItemDetailContainer = () => {
 
     
     return (
-      <div className=''>
-<div className="div"
-disabled={isLoading}>{isLoading ? <Spinners/>: <ItemDetail producto={producto} key={producto.id} />}
-</div>
+      <>
+      <div className='containerfluid' >
+<div className="row " >
+<div className='col-12 justify-content-center d-flex  '
+ disabled={isLoading}>{isLoading ? <Dna
+  visible={true}
+  height="80"
+  width="80"
+  paddingTop="70"
+  ariaLabel="dna-loading"
+  wrapperStyle={{margin:"20rem"}}
 
+
+  wrapperClass="dna-wrapper"/>: <ItemDetail producto={producto} key={producto.id} />}
+</div>
+</div>
         
       </div>
 
-
+      </>
 
 
   );
